4b1661c9ba4ba2ee95c5b762f8488ecf25d154ad
Sharjeel <mshijiaslam@gmail.com>
2025-09-03 16:42:11 +0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,ts,jsx,tsx}',
+//    './src/index.html',
+//    '/src/index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
